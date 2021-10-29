5197d520678e7cd06462e1dc46c658fca4653518
Rohit Kumar Singh <rohitt.singh15@gmail.com>
2021-10-29 11:35:57 +0530

    adding bean validation -closes: #8

--- src/main/java/com/developer/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/developer/springframework/msscbeerservice/web/model/BeerDto.java
@@ -5,32 +5,47 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
 /*
-* Created by Rohit on 25-10-2021
-*/
+ * Created by Rohit on 25-10-2021
+ */
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class BeerDto {
-private UUID idl;
-private Integer version;
+    @Null
+    private UUID id;
+    @Null
+    private Integer version;
 
-private OffsetDateTime createdDate;
-private OffsetDateTime lastModifiedDate;
+    @Null
+    private OffsetDateTime createdDate;
+    @Null
+    private OffsetDateTime lastModifiedDate;
 
-private String beerName;
-private BeerStyleEnum beerStyle;
+    @NotBlank
+    private String beerName;
 
-private Long upc;
+    @NotNull
+    private BeerStyleEnum beerStyle;
 
-private BigDecimal price;
+    @Positive
+    @NotNull
+    private Long upc;
 
-private Integer quantityOrder;
+    @Positive
+    @NotNull
+    private BigDecimal price;
+
+    private Integer quantityOrder;
 }
 
--- src/test/java/com/developer/springframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/developer/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -5,13 +5,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -44,9 +49,19 @@ class BeerControllerTest {
     void updateBeer() throws Exception {
         BeerDto beerDto = BeerDto.builder().build();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
-        mockMvc.perform(put("/api/v1/beer/"+UUID.randomUUID().toString())
+        mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(beerDtoJson))
                 .andExpect(status().isNoContent());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List> validationErrorHandling(ConstraintViolationException e) {
+        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
+        e.getConstraintViolations().forEach(constraintVoilation -> {
+            errors.add(constraintVoilation.getPropertyPath() + " : " + constraintVoilation.getMessage());
+
+        };
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
